7679bb0e701e2259161f6a12ca258ad57ca33eec
Megha Mahna <meghamahna04@gmail.com>
2020-01-15 19:38:05 -0500

    Initial Commit

---
+++ IOS_769247_Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_769247_Assignment1
+//
+//  Created by Megha Mahna on 2020-01-15.
+//  Copyright © 2020 meghamahna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
